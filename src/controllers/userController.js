8506d8d07a7a8f26ba1acb16db65f25fe12848f3
miguelfiais <miguel.fiais10@gmail.com>
2023-05-02 19:48:48 -0300

    feat: encrypt password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,14 @@
 import { createUser } from "../repositorys/userRepository";
-
+import bcrypt from "bcrypt";
 export const store = async (req, res) => {
   try {
-    const user = await createUser(req.body);
+    const { name, email, password } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await createUser({
+      name,
+      email,
+      password: hashPassword,
+    });
     return res.status(201).json(user);
   } catch (error) {
     return res.status(400).json({ error });
